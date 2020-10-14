a4d89ece891f3ae7349ae68a029fded9fbc07cb1
Andras Orban <andras.orban@hu.bosch.com>
2020-10-14 18:16:25 +0200

    Add .prettierrc file and restore accidentally changed component

--- src/components/ReactResponsiveCarousel/ReactResponsiveCarousel.js
+++ src/components/ReactResponsiveCarousel/ReactResponsiveCarousel.js
@@ -1,39 +1,39 @@
-import React from "react";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import Title from "../Title/Title";
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Title from '../Title/Title';
 
 const ReactResponsiveCarousel = () => {
-  return (
-    <section>
-      <Title
-        title="React Responsive Carousel"
-        href="https://www.npmjs.com/package/react-responsive-carousel"
-      />
-      <Carousel>
-        <div>
-          <img src="https://picsum.photos/1920/700?random=1" alt="" />
-          <p className="legend">This is the first random image.</p>
-        </div>
-        <div>
-          <img src="https://picsum.photos/1920/700?random=2" alt="" />
-          <p className="legend">This is the second random image.</p>
-        </div>
-        <div>
-          <img src="https://picsum.photos/1920/700?random=3" alt="" />
-          <p className="legend">This is the third random image.</p>
-        </div>
-        <div>
-          <img src="https://picsum.photos/1920/700?random=4" alt="" />
-          <p className="legend">This is the fourth random image.</p>
-        </div>
-        <div>
-          <img src="https://picsum.photos/1920/700?random=5" alt="" />
-          <p className="legend">This is the fifth random image.</p>
-        </div>
-      </Carousel>
-    </section>
-  );
+    return (
+        <section>
+            <Title
+                title="React Responsive Carousel"
+                href="https://www.npmjs.com/package/react-responsive-carousel"
+            />
+            <Carousel>
+                <div>
+                    <img src="https://picsum.photos/1920/700?random=1" alt="" />
+                    <p className="legend">This is the first random image.</p>
+                </div>
+                <div>
+                    <img src="https://picsum.photos/1920/700?random=2" alt="" />
+                    <p className="legend">This is the second random image.</p>
+                </div>
+                <div>
+                    <img src="https://picsum.photos/1920/700?random=3" alt="" />
+                    <p className="legend">This is the third random image.</p>
+                </div>
+                <div>
+                    <img src="https://picsum.photos/1920/700?random=4" alt="" />
+                    <p className="legend">This is the fourth random image.</p>
+                </div>
+                <div>
+                    <img src="https://picsum.photos/1920/700?random=5" alt="" />
+                    <p className="legend">This is the fifth random image.</p>
+                </div>
+            </Carousel>
+        </section>
+    );
 };
 
 export default ReactResponsiveCarousel;
